88955cb20b682b045063ab5e22800d396715c11e
Ivan <ivan120536@gmail.com>
2023-04-19 18:26:31 +0400

    added error templates, pico-css styling

---
+++ error_handlers.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template
+from werkzeug.exceptions import HTTPException
+
+errors = {
+ 404 : {
+    "number" : 404,
+    "name" : "Страница не найдена",
+    "description" : "Вероятнее всего вы попали не на ту страницу"
+ },
+ 400 : {
+   "number" : 400,
+   "name" : "Неверный запрос",
+   "description" : "Вероятнее всего вы неверно оформили запрос"
+ }
+}
+
+error_blueprint = Blueprint("error_page", __name__)
+
+   
+@error_blueprint.app_errorhandler(Exception)
+def handle_error(e):
+   if isinstance(e, HTTPException):
+      error = errors[e.code]
+      return render_template("error.html.j2", **error), error["number"]
+   else:
+      return 'Something really fucked up', e
--- main.py
+++ main.py
@@ -2,20 +2,18 @@ from flask import Flask, request, render_template
 
 from data import db_session
 
+from error_handlers import error_blueprint
+
 app = Flask(__name__)
-
-@app.errorhandler(400)
-def handle_invalid_request(e):
-   pass 
-
 
 @app.route('/', methods=["GET"])
 def index():
-    return "Hello, wordlie~!"
+    return render_template("index.html.j2")
 
 def main() -> None:
     db_session.global_init("db/main_db.sqlite")
-    app.run(host='0.0.0.0', port=8080)    
+    app.register_blueprint(error_blueprint)
+    app.run(host='0.0.0.0', port=8080)
 
 if __name__ == "__main__":
     main()
